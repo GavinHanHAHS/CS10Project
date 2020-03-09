0243c4c5558a139691ea69e2c734ec4a93db1e0c
GavinHanHAHS <55220804+GavinHanHAHS@users.noreply.github.com>
2020-03-09 09:26:10 -0600

    Update main.js

--- gamesnthings/bubbleBubble/main.js
+++ gamesnthings/bubbleBubble/main.js
@@ -2,7 +2,86 @@
 
 let cnv = document.getElementById("mainCanvas");
 let ctx = cnv.getContext("2d");
+let mouseX;
+let mouseY;
+
 
 cnv.width = 900;
 cnv.height = 600;
 
+let potionBookUp = false;
+
+document.addEventListener("mousemove", mousemoveHandler);
+document.addEventListener("click", clickHandler);
+
+
+requestAnimationFrame(mainLoop);
+
+function mainLoop() {
+
+  //draw background
+  drawBackground();
+
+  //potion book drawing and logic
+  potionBook();
+
+  //potion crafting logic
+  potionMagic();
+
+  
+
+
+  requestAnimationFrame(mainLoop);
+}
+
+function drawBackground() {
+
+  //draw background
+  ctx.fillStyle = "darkGrey";
+  ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+  ctx.fillStyle = "grey";
+  ctx.beginPath();
+  ctx.arc(cnv.width / 2, 400, 100, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+function potionMagic() {
+  //logic for potion ingredients and potion brewing
+}
+
+function potionBook() {
+  //drawing potion book
+
+  ctx.fillStyle = "green";
+  ctx.fillRect(700, 550, 195, 45);
+
+  if(!potionBookUp) {
+    ctx.fillStyle = "lime";
+    ctx.font = "35px Arial";
+    ctx.fillText("Open Book", 710, 585);
+  } else {
+    ctx.fillStyle = "lime";
+    ctx.font = "35px Arial";
+    ctx.fillText("Close Book", 710, 585);
+
+    ctx.fillStyle = "brown";
+    ctx.fillRect(100, 100, 700, 400);
+  }
+
+  
+
+  //potion book page flip logic
+}
+
+function mousemoveHandler() {
+  let cnvRect = cnv.getBoundingClientRect(); //check could be run only once if game lags
+  mouseX = event.clientX - cnvRect.left;
+  mouseY = event.clientY - cnvRect.top;
+}
+
+function clickHandler() {
+  if(mouseX >= 700 && mouseX <= 895 && mouseY >= 550 && mouseY <= 595) {
+    potionBookUp = !potionBookUp;
+  }
+}
