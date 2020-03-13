fd2555e9319d5a5cf62d7185074956f65bc84b7e
GavinHanHAHS <55220804+GavinHanHAHS@users.noreply.github.com>
2020-03-13 09:33:47 -0600

    implement some functionality to potion brewing

--- gamesnthings/bubbleBubble/main.js
+++ gamesnthings/bubbleBubble/main.js
@@ -13,6 +13,28 @@ let potionBookUp = false;
 let potionBookHeight = 700;
 let bookSpeed = 5;
 
+let ingredientFalling = false;
+let ingredientHeight = -300;
+let ingredientSpeed = 0;
+
+let potionCase = 0;
+
+let image = {
+  bookInside: document.getElementById("bookInside"),
+  slime: document.getElementById("slime"),
+  eyeball: document.getElementById("eyeball"),
+  batWing: document.getElementById("batWing"),
+  batWingCocktail: document.getElementById("batWingCocktail")
+};
+
+let recipes = {
+  batWingCocktail: ["slime", "eyeball", "batWing"]
+};
+
+let ingredients = [];
+let targetPotion;
+
+
 document.addEventListener("mousemove", mousemoveHandler);
 document.addEventListener("click", clickHandler);
 
@@ -23,6 +45,8 @@ function mainLoop() {
 
   //draw background
   drawBackground();
+
+  drawIngredients();
 
   //potion book drawing and logic
   potionBook();
@@ -50,10 +74,37 @@ function drawBackground() {
   //draw potion book
   ctx.fillStyle = "brown";
   ctx.fillRect(100, potionBookHeight, 700, 400);
+
+  ctx.drawImage(image.bookInside, 0, potionBookHeight - 30, 875, 470);
+  
 }
 
 function potionMagic() {
   //logic for potion ingredients and potion brewing
+  if(!targetPotion) {
+    //random num generator for random potion
+    let rand = 1;
+    switch(rand) {
+      case 1:
+        targetPotion = JSON.stringify(recipes.batWingCocktail);
+        dropIngredients("batWingCocktail");
+        console.log(targetPotion);
+        break;
+      default:
+        targetPotion = "error";
+        console.log("error");
+    }
+  }
+
+  // if(ingredientFalling) {
+  //   if(!(ingredientHeight >= 50)) {
+  //     ingredientHeight += ingredientSpeed;
+  //     ingredientSpeed += 0.5;
+  //   } else {
+  //     ingredientSpeed = 0;
+  //   }
+  // }
+  
 }
 
 function potionBook() {
@@ -87,9 +138,46 @@ function potionBook() {
     
   }
 
+  //potion book page flip logic
+}
+
+function dropIngredients(potion) { 
+  //random number function
+  let rand = 1;
+  console.log("ingredients dropping");
+  console.log(potion);
+  if(potion == "batWingCocktail") {
+    console.log("batWingCocktail");
+    switch(rand) {
+      case 1: 
+        //draw image 1 as slime, image 2 as eyeball, and image 3 as bat wing.
+        console.log("ingredients: slime, eyeball, batwing");
+        ingredientFalling = true;
+        potionCase = 1;
+        break; // add more cases for more variation.
+      default:
+        console.log("error: dropIngredients invalid random num (" + potion + ")");
+    }
+  }
   
 
-  //potion book page flip logic
+}
+
+function drawIngredients() {
+  if(ingredientFalling == true) {
+    if(targetPotion == JSON.stringify(recipes.batWingCocktail)) {
+      if(potionCase == 1) { //use potionCase to determine which potion you click when detecting that
+        ctx.drawImage(image.slime, 50, ingredientHeight);
+        ctx.drawImage(image.eyeball, 350, ingredientHeight);
+        ctx.drawImage(image.batWing, 650, ingredientHeight);
+      }
+    }
+
+    if(ingredientHeight <= 50) {
+      ingredientHeight += ingredientSpeed;
+      ingredientSpeed += 0.1;
+    }
+  }
 }
 
 function mousemoveHandler() {
